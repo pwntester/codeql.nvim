b43542e52dbcafb0db03568fae36f9453637df2d
Alvaro Muñoz Sanchez <alvaro@pwntester.com>
2021-11-13 09:43:24 +0100

    style changes

--- lua/codeql/init.lua
+++ lua/codeql/init.lua
@@ -1,5 +1,5 @@
-local util = require'codeql.util'
-local queryserver = require'codeql.queryserver'
+local util = require "codeql.util"
+local queryserver = require "codeql.queryserver"
 local vim = vim
 local api = vim.api
 local format = string.format
@@ -12,41 +12,42 @@ vim.g.codeql_ram_opts = {}
 M.count = 0
 
 function M.load_definitions()
-
   local bufnr = vim.api.nvim_get_current_buf()
   local bufname = vim.fn.bufname(bufnr)
 
   -- not a codeql:// buffer
-  if not vim.startswith(bufname, 'codeql:/') then return end
+  if not vim.startswith(bufname, "codeql:/") then
+    return
+  end
 
   -- check if file has already been processed
-  local defs = require'codeql.defs'
-  local fname = vim.split(bufname, ':')[2]
+  local defs = require "codeql.defs"
+  local fname = vim.split(bufname, ":")[2]
   if defs.processedFiles[fname] then
     return
   end
 
   -- query the buffer for defs and refs
-  M.run_templated_query('localDefinitions', fname)
-  M.run_templated_query('localReferences', fname)
+  M.run_templated_query("localDefinitions", fname)
+  M.run_templated_query("localReferences", fname)
 
   -- prevent further definition queries from being run on the same buffer
   defs.processedFiles[fname] = true
 end
 
 function M.set_database(dbpath)
-  local database = vim.fn.fnamemodify(vim.trim(dbpath), ':p')
-  if not vim.endswith(database, '/') then
-    database = database..'/'
+  local database = vim.fn.fnamemodify(vim.trim(dbpath), ":p")
+  if not vim.endswith(database, "/") then
+    database = database .. "/"
   end
   if not util.is_dir(database) then
-    util.err_message('Incorrect database: '..database)
+    util.err_message("Incorrect database: " .. database)
   else
     local metadata = util.database_info(database)
     metadata.path = database
-    api.nvim_set_var('codeql_database', metadata)
+    api.nvim_set_var("codeql_database", metadata)
     queryserver.register_database()
-    util.message('Database set to '..database)
+    util.message("Database set to " .. database)
   end
   --TODO: print(util.database_upgrades(vim.g.codeql_database.dbscheme))
   vim.g.codeql_ram_opts = util.resolve_ram(true)
@@ -55,18 +56,17 @@ end
 function M.run_query(quick_eval)
   local db = vim.g.codeql_database
   if not db then
-    util.err_message('Missing database. Use :SetDatabase command')
+    util.err_message "Missing database. Use :SetDatabase command"
     return
   end
 
   local dbPath = vim.g.codeql_database.path
-  local queryPath = vim.fn.expand('%:p')
+  local queryPath = vim.fn.expand "%:p"
 
   -- [bufnum, lnum, col, off, curswant]
   local line_start, column_start, line_end, column_end
 
-  if vim.fn.getpos("'<")[2] == vim.fn.getcurpos()[2] and
-    vim.fn.getpos("'<")[3] == vim.fn.getcurpos()[3] then
+  if vim.fn.getpos("'<")[2] == vim.fn.getcurpos()[2] and vim.fn.getpos("'<")[3] == vim.fn.getcurpos()[3] then
     line_start = vim.fn.getpos("'<")[2]
     column_start = vim.fn.getpos("'<")[3]
     line_end = vim.fn.getpos("'>")[2]
@@ -83,31 +83,31 @@ function M.run_query(quick_eval)
   local libPaths = util.resolve_library_path(queryPath)
 
   local opts = {
-    quick_eval = quick_eval;
-    bufnr = api.nvim_get_current_buf();
-    query = queryPath;
-    dbPath = dbPath;
-    startLine = line_start;
-    startColumn = column_start;
-    endLine = line_end;
-    endColumn = column_end;
-    metadata = util.query_info(queryPath);
-    libraryPath = libPaths.libraryPath;
-    dbschemePath = libPaths.dbscheme;
+    quick_eval = quick_eval,
+    bufnr = api.nvim_get_current_buf(),
+    query = queryPath,
+    dbPath = dbPath,
+    startLine = line_start,
+    startColumn = column_start,
+    endLine = line_end,
+    endColumn = column_end,
+    metadata = util.query_info(queryPath),
+    libraryPath = libPaths.libraryPath,
+    dbschemePath = libPaths.dbscheme,
   }
 
   queryserver.run_query(opts)
 end
 
 local templated_queries = {
-  c          = 'cpp/ql/src/%s.ql';
-  cpp        = 'cpp/ql/src/%s.ql';
-  java       = 'java/ql/src/%s.ql';
-  cs         = 'csharp/ql/src/%s.ql';
-  go         = 'ql/src/%s.ql';
-  javascript = 'javascript/ql/src/%s.ql';
-  python     = 'python/ql/src/%s.ql';
-  ruby       = 'ql/src/ide-contextual-queries/%s.ql';
+  c = "cpp/ql/src/%s.ql",
+  cpp = "cpp/ql/src/%s.ql",
+  java = "java/ql/src/%s.ql",
+  cs = "csharp/ql/src/%s.ql",
+  go = "ql/src/%s.ql",
+  javascript = "javascript/ql/src/%s.ql",
+  python = "python/ql/src/%s.ql",
+  ruby = "ql/src/ide-contextual-queries/%s.ql",
 }
 
 function M.run_print_ast()
@@ -115,16 +115,18 @@ function M.run_print_ast()
   local bufname = vim.fn.bufname(bufnr)
 
   -- not a codeql:// buffer
-  if not vim.startswith(bufname, 'codeql:/') then return end
+  if not vim.startswith(bufname, "codeql:/") then
+    return
+  end
 
-  local fname = vim.split(bufname, ':')[2]
-  M.run_templated_query('printAst', fname)
+  local fname = vim.split(bufname, ":")[2]
+  M.run_templated_query("printAst", fname)
 end
 
 function M.run_templated_query(query_name, param)
   local bufnr = api.nvim_get_current_buf()
   local dbPath = vim.g.codeql_database.path
-  local ft = vim.bo[bufnr]['ft']
+  local ft = vim.bo[bufnr]["ft"]
   if not templated_queries[ft] then
     --util.err_message(format('%s does not support %s file type', query_name, ft))
     return
@@ -132,37 +134,36 @@ function M.run_templated_query(query_name, param)
   local query = format(templated_queries[ft], query_name)
   local queryPath
   for _, path in ipairs(vim.g.codeql_search_path) do
-    local candidate = format('%s/%s', path, query)
-    print(1, candidate)
+    local candidate = format("%s/%s", path, query)
     if util.is_file(candidate) then
       queryPath = candidate
       break
     end
   end
   if not queryPath then
-    vim.notify(format('Cannot find a valid %s query', query_name), 2)
+    --vim.notify(format('Cannot find a valid %s query', query_name), 2)
     return
   end
 
   local templateValues = {
     selectedSourceFile = {
       values = {
-        tuples = { { { stringValue = param; } } }
-        }
-      }
+        tuples = { { { stringValue = param } } },
+      },
+    },
   }
   local libPaths = util.resolve_library_path(queryPath)
   local opts = {
-    quick_eval = false;
-    bufnr = bufnr;
-    query = queryPath;
-    dbPath = dbPath;
-    metadata = util.query_info(queryPath);
-    libraryPath = libPaths.libraryPath;
-    dbschemePath = libPaths.dbscheme;
-    templateValues = templateValues;
+    quick_eval = false,
+    bufnr = bufnr,
+    query = queryPath,
+    dbPath = dbPath,
+    metadata = util.query_info(queryPath),
+    libraryPath = libPaths.libraryPath,
+    dbschemePath = libPaths.dbscheme,
+    templateValues = templateValues,
   }
-  require'codeql.queryserver'.run_query(opts)
+  require("codeql.queryserver").run_query(opts)
 end
 
 return M
