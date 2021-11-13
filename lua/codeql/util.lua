b42be3280a6621eff645937cc18e9da3d7988ad3
Alvaro Muñoz Sanchez <alvaro@pwntester.com>
2021-11-13 09:40:44 +0100

    Window picker

--- lua/codeql/util.lua
+++ lua/codeql/util.lua
@@ -231,4 +231,114 @@ function M.tbl_slice(tbl, s, e)
   return new
 end
 
+function M.get_user_input_char()
+  local c = vim.fn.getchar()
+  while type(c) ~= "number" do
+    c = vim.fn.getchar()
+  end
+  return vim.fn.nr2char(c)
+end
+
+local special_buffers = {
+  filetype = {
+    "help",
+    "fortifytestpane",
+    "fortifyauditpane",
+    "qf",
+    "goterm",
+    "codeqlpanel",
+    "terminal",
+    "packer",
+    "NvimTree",
+    "octo",
+    "octo_panel",
+    "aerieal",
+    "Trouble",
+    "dashboard",
+    "frecency",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "NeogitStatus",
+    "notify",
+  }
+}
+
+---Get user to pick a window. Selectable windows are all windows in the current
+---tabpage that aren't NvimTree.
+---@return integer|nil -- If a valid window was picked, return its id. If an
+---       invalid window was picked / user canceled, return nil. If there are
+---       no selectable windows, return -1.
+--- from: https://github.com/kyazdani42/nvim-tree.lua/blob/master/lua/nvim-tree/lib.lua
+function M.pick_window(panel_winid)
+  local tabpage = api.nvim_get_current_tabpage()
+  local win_ids = api.nvim_tabpage_list_wins(tabpage)
+  local exclude = special_buffers
+
+  local selectable = vim.tbl_filter(function (id)
+    local bufid = api.nvim_win_get_buf(id)
+    for option, v in pairs(exclude) do
+      local ok, option_value = pcall(api.nvim_buf_get_option, bufid, option)
+      if ok and vim.tbl_contains(v, option_value) then
+        return false
+      end
+    end
+
+    local win_config = api.nvim_win_get_config(id)
+    return id ~= panel_winid
+      and win_config.focusable
+      and not win_config.external
+  end, win_ids)
+
+  -- If there are no selectable windows: return. If there's only 1, return it without picking.
+  if #selectable == 0 then return -1 end
+  if #selectable == 1 then return selectable[1] end
+
+  local chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+  local i = 1
+  local win_opts = {}
+  local win_map = {}
+  local laststatus = vim.o.laststatus
+  vim.o.laststatus = 2
+
+  -- Setup UI
+  for _, id in ipairs(selectable) do
+    local char = chars:sub(i, i)
+    local ok_status, statusline = pcall(api.nvim_win_get_option, id, "statusline")
+    local ok_hl, winhl = pcall(api.nvim_win_get_option, id, "winhl")
+
+    win_opts[id] = {
+      statusline = ok_status and statusline or "",
+      winhl = ok_hl and winhl or ""
+    }
+    win_map[char] = id
+
+    api.nvim_win_set_option(id, "statusline", "%=" .. char .. "%=")
+    api.nvim_win_set_option(
+      id, "winhl", "StatusLine:CodeQLWindowPicker,StatusLineNC:CodeQLWindowPicker"
+    )
+
+    i = i + 1
+    if i > #chars then break end
+  end
+
+  vim.cmd("redraw")
+  print("Pick window: ")
+  local _, resp = pcall(M.get_user_input_char)
+  resp = (resp or ""):upper()
+  -- clears prompt
+  vim.api.nvim_command('normal! :')
+
+  -- Restore window options
+  for _, id in ipairs(selectable) do
+    for opt, value in pairs(win_opts[id]) do
+      api.nvim_win_set_option(id, opt, value)
+    end
+  end
+
+  vim.o.laststatus = laststatus
+
+  return win_map[resp]
+end
+
 return M
