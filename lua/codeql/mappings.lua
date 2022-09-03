08fd1b752a83fe34d101d997cd778d306ee47e49
Alvaro Muñoz Sanchez <alvaro@pwntester.com>
2022-09-03 23:15:19 +0200

    add mappings.lua file

---
+++ lua/codeql/mappings.lua
@@ -0,0 +1,5 @@
+return {
+  run_query = "<Cmd>lua require 'codeql'.run_query()<CR>",
+  quick_eval = "<Cmd>lua require 'codeql'.quick_evaluate()<CR>",
+  quick_eval_predicate = "<Cmd>lua require 'codeql'.quick_evaluate_enclosing_predicate()<CR>",
+}
